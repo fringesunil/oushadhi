46f4a553301ea5b9c692e31f15ee545e696f4a05
Fringe sunil <fringeks011@gmail.com>
2025-11-17 23:18:56 +0530

    Login Screen

---
+++ lib/Core/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+   static const Color primaryGreen = Color(0xFF37B56A); 
+  static const Color lightAccent = Color(0xFF9BE7B4); 
+  static const Color bgLightGreen = Color(0xFFF3FBF6); 
+  static const Color white = Colors.white;
+}
